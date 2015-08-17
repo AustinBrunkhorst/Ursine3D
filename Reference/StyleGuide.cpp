b3e5cb15b100c9532559685ec4ff26dd9b3e912d
Austin Brunkhorst <contact@austinbrunkhorst.com>
2015-08-17 12:23:27 -0700

    Jokes on you, indentation is with spaces, not tabs

--- Reference/StyleGuide.cpp
+++ Reference/StyleGuide.cpp
@@ -14,156 +14,156 @@
 // lowercase, words delimited by underscore (no camel case)
 namespace namespace_name
 {
-	// uppercase, camel case
-	class ClassName
-		, DerivedClass1
-		, DerivedClass2
-		, LastDerivedClass
-	{
-	// public should always come first in header declarations
-	public:
-		// lowercase, camel case
-		double publicField;
+    // uppercase, camel case
+    class ClassName
+        , DerivedClass1
+        , DerivedClass2
+        , LastDerivedClass
+    {
+    // public should always come first in header declarations
+    public:
+        // lowercase, camel case
+        double publicField;
 
-		ClassName(void);
+        ClassName(void);
 
-		// void must be included in no-argument functions
-		void PublicMethod(void) const;
+        // void must be included in no-argument functions
+        void PublicMethod(void) const;
 
-		// & and * couple argument names
-		int PublicMethod(const int &arg1, double *arg2);
+        // & and * couple argument names
+        int PublicMethod(const int &arg1, double *arg2);
 
-		// example function header comment - basic doxygen
+        // example function header comment - basic doxygen
 
-		/** @brief Prints the string str, starting at the current
-		 *         location of the cursor.
-		 *
-		 *  If the string is longer than the current line, the
-		 *  string should fill up the current line and then
-		 *  continue on the next line. If the string exceeds
-		 *  available space on the entire console, the screen
-		 *  should scroll up one line, and then the string should
-		 *  continue on the new line.  If '\n', '\r', and '\b' are
-		 *  encountered within the string, they should be handled
-		 *  as per putbyte. If length is not a positive integer or str
-		 *  is null, the function has no effect.
-		 *
-		 *  @param str The string to be printed.
-		 *  @param length The length of the string str.
-		 *  @return Void.
-		 */
-		void PutBytes(const char *str, int length);
+        /** @brief Prints the string str, starting at the current
+         *         location of the cursor.
+         *
+         *  If the string is longer than the current line, the
+         *  string should fill up the current line and then
+         *  continue on the next line. If the string exceeds
+         *  available space on the entire console, the screen
+         *  should scroll up one line, and then the string should
+         *  continue on the new line.  If '\n', '\r', and '\b' are
+         *  encountered within the string, they should be handled
+         *  as per putbyte. If length is not a positive integer or str
+         *  is null, the function has no effect.
+         *
+         *  @param str The string to be printed.
+         *  @param length The length of the string str.
+         *  @return Void.
+         */
+        void PutBytes(const char *str, int length);
 
-	// protected follows the same style as private
-	private:
-		// same as public, but prefixed with "m_"
-		int m_privateField;
+    // protected follows the same style as private
+    private:
+        // same as public, but prefixed with "m_"
+        int m_privateField;
 
-		std::string m_anotherPrivate;
+        std::string m_anotherPrivate;
 
-		// same as public fields
-		void privateMethod(void);
-	};
+        // same as public fields
+        void privateMethod(void);
+    };
 
-	// constructor initializer lists should follow this format
-	ClassName::ClassName(void)
-		: publicField( 3.2 )
-		, m_privateField( 4 )
-		, m_anotherPrivate( "spacing, man." ) 
-	{
+    // constructor initializer lists should follow this format
+    ClassName::ClassName(void)
+        : publicField( 3.2 )
+        , m_privateField( 4 )
+        , m_anotherPrivate( "spacing, man." ) 
+    {
 
-	}
+    }
 }
 
 // global functions follow the same naming convention as classes
 void GlobalFunction(void) 
 { 
-	bool test = false;
+    bool test = false;
 
-	if (test) 
-	{
-		// braces are on newlines
-	}
+    if (test) 
+    {
+        // braces are on newlines
+    }
 
-	// all language constructs have spaces after the keyword and 
-	// before the open parentheses
+    // all language constructs have spaces after the keyword and 
+    // before the open parentheses
 
-	for (;;);
-	while (true);
-	do { } while (true);
+    for (;;);
+    while (true);
+    do { } while (true);
 }
 
 // please keep comments lowercase
 // <-- also keep one space after comment declarations
 int main(void)
 {
-	// always use "f" when describing float literals
-	float decimal = 19.45f;
+    // always use "f" when describing float literals
+    float decimal = 19.45f;
 
-	// always keep at least a 0 for the fractional part of any floating point
-	double otherDecimal = 15.0; // 15, 15., 15.f : no no
+    // always keep at least a 0 for the fractional part of any floating point
+    double otherDecimal = 15.0; // 15, 15., 15.f : no no
 
-	namespace_name::ClassName myClass;
+    namespace_name::ClassName myClass;
 
-	// function invocation arguments are padded
-	// with spaces
+    // function invocation arguments are padded
+    // with spaces
 
-	myClass.PublicMethod( );
+    myClass.PublicMethod( );
 
-	myClass.PublicMethod( 1, &otherDecimal );
+    myClass.PublicMethod( 1, &otherDecimal );
 
-	int justAnArray[10];
+    int justAnArray[10];
 
-	// index operators are also padded
-	int value = justAnArray[ 3 ];
+    // index operators are also padded
+    int value = justAnArray[ 3 ];
 
-	// prefer prefix increment/decrement over postfix
-	for (int i = 0; i < 10; ++i) { }
+    // prefer prefix increment/decrement over postfix
+    for (int i = 0; i < 10; ++i) { }
 
-	// prefer initializer list constructors
-	std::vector<int> justAVector { 1, 2, 3, 4 };
+    // prefer initializer list constructors
+    std::vector<int> justAVector { 1, 2, 3, 4 };
 
-	// prefer range-based for loops when possible
-	for (auto value : justAVector) { }
+    // prefer range-based for loops when possible
+    for (auto value : justAVector) { }
 
-	// absolutely use "auto" type deduction when
-	// possible.
-	auto anotherValue = justAVector[ 2 ];
+    // absolutely use "auto" type deduction when
+    // possible.
+    auto anotherValue = justAVector[ 2 ];
 
-	// use */& coupled with these types
-	// when using auto
-	auto *ptrValue = &justAVector[ 0 ];
-	auto &refValue = justAVector[ 1 ];
+    // use */& coupled with these types
+    // when using auto
+    auto *ptrValue = &justAVector[ 0 ];
+    auto &refValue = justAVector[ 1 ];
 
-	// use a lot of newlines so sections of code don't get too cluttered
+    // use a lot of newlines so sections of code don't get too cluttered
 
-	for (auto value : justAVector)
-	{
-		if (value >= 5 && *ptrValue == 3)
-		{
-			myClass.PublicMethod( );
+    for (auto value : justAVector)
+    {
+        if (value >= 5 && *ptrValue == 3)
+        {
+            myClass.PublicMethod( );
 
-			justAnArray[ 3 ] = 3;
+            justAnArray[ 3 ] = 3;
 
-			justAVector.push_back( 9001 );
-		}
+            justAVector.push_back( 9001 );
+        }
 
-		refValue = 6;
-	}
+        refValue = 6;
+    }
 
-	// UNDESIRABLE
+    // UNDESIRABLE
 
-	for (auto value : justAVector)
-	{
-		if (value >= 5 && *ptrValue == 3)
-		{
-			myClass.PublicMethod( );
-			justAnArray[ 3 ] = 3;
-			justAVector.push_back( 9001 );
-		}
-		refValue = 6;
-	}
+    for (auto value : justAVector)
+    {
+        if (value >= 5 && *ptrValue == 3)
+        {
+            myClass.PublicMethod( );
+            justAnArray[ 3 ] = 3;
+            justAVector.push_back( 9001 );
+        }
+        refValue = 6;
+    }
 
-	// treat your code as a piece of art, because it is.
-	// we're going for consistency in addition to readability
+    // treat your code as a piece of art, because it is.
+    // we're going for consistency in addition to readability
 }
