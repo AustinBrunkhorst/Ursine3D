091aad13e373bec21a887a6776bbe39ba9d4e811
Jordan Ellis <jordanellis6809@gmail.com>
2015-08-28 00:44:03 -0700

    Unity project changes

---
+++ Source/Projects/Tests/Math/Source/QuaternionTests.cpp
@@ -0,0 +1,5 @@
+#include "UrsinePrecompiled.h"
+
+/*http://lolengine.net/browser/trunk/test/unit/quat.cpp?rev=3293
+
+http://commons.apache.org/proper/commons-math/xref-test/org/apache/commons/math3/complex/QuaternionTest.html*/
