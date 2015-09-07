e7be590012d61b2fe80af083aa0b916fa176f03e
Austin Brunkhorst <contact@austinbrunkhorst.com>
2015-09-07 14:10:28 -0700

    Variant const for generic type.

--- Source/UrsineEngine/include/Meta/Impl/Variant.hpp
+++ Source/UrsineEngine/include/Meta/Impl/Variant.hpp
@@ -18,7 +18,7 @@ namespace ursine
 
         template<typename T>
         Variant::Variant(T &data)
-            : m_isConst( false )
+            : m_isConst( std::is_pointer<T>::value && std::is_const<T>::value )
             , m_base( new VariantContainer< CleanedType<T> >( data ) )
         {
         
