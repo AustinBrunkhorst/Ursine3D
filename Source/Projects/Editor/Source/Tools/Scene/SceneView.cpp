555c4ab32c11de55c20bc5c4ec8adaf9ee6e3e1f
Jordan Ellis <jordanellis6809@gmail.com>
2015-10-01 15:08:04 -0700

    camera controller, skybox, other stuff

--- Source/Projects/Editor/Source/Tools/Scene/SceneView.cpp
+++ Source/Projects/Editor/Source/Tools/Scene/SceneView.cpp
@@ -17,21 +17,22 @@ SceneView::SceneView(JSHandlerArgs)
         .On( "viewportInvalidated", &SceneView::onViewportInvalidated );
 
     GetCoreSystem( MouseManager )->Listener( this )
-        .On( MM_SCROLL, &SceneView::onMouseScroll )
-        .On( MM_MOVE, &SceneView::onMouseMove );
+        .On( MM_SCROLL, &SceneView::onMouseScroll );
 
-    GetCoreSystem( KeyboardManager )->Listener( this )
-        .On( KM_KEY_DOWN, &SceneView::onKeyDown );
+    Application::Instance->Listener( this )
+        .On( APP_UPDATE, &SceneView::onAppUpdate );
 }
 
 SceneView::~SceneView(void)
 {
-    GetCoreSystem( MouseManager )->Listener( this )
-        .Off( MM_SCROLL, &SceneView::onMouseScroll )
-        .Off( MM_MOVE, &SceneView::onMouseMove );
+    Listener(this)
+        .Off("viewportInvalidated", &SceneView::onViewportInvalidated);
 
-    GetCoreSystem( KeyboardManager )->Listener( this )
-        .Off( KM_KEY_DOWN, &SceneView::onKeyDown );
+    GetCoreSystem( MouseManager )->Listener( this )
+        .Off( MM_SCROLL, &SceneView::onMouseScroll );
+
+    Application::Instance->Listener(this)
+        .Off(APP_UPDATE, &SceneView::onAppUpdate);
 }
 
 GFXCamera &SceneView::getEditorCamera(void)
@@ -39,6 +40,40 @@ GFXCamera &SceneView::getEditorCamera(void)
     return GetCoreSystem( GfxAPI )->CameraMgr.GetCamera( 
         m_editor->GetProject( )->GetScene( ).GetEditorCamera( )
     );
+}
+
+void SceneView::onAppUpdate(EVENT_HANDLER(ursine::Application))
+{
+    EVENT_ATTRS( Application, EventArgs );
+
+    if (!IsFocused())
+        return;
+
+    auto app = Application::Instance;
+    auto dt = app->GetDeltaTime( );
+
+    auto *keyboardMgr = GetCoreSystem(KeyboardManager);
+
+    // first, update all of the camera stuff. 
+    //keyboard controls
+    UpdateCameraKeys(dt);
+
+    // mouse controls
+    if (keyboardMgr->GetModifiers() & KMD_ALT)
+    {
+        UpdateCameraMouse(dt);
+    }
+
+    //our position always needs to be relative to the center position
+    GFXCamera &cam = getEditorCamera();
+    SVec3 look = cam.GetLook();
+
+    //normalize look and scale by zoom
+    look.Normalize();
+    look = look * m_camZoom;
+
+    //negate the vector, opposite of look is going away from center
+    cam.SetPosition(m_camPos - look);
 }
 
 void SceneView::onViewportInvalidated(EVENT_HANDLER(NativeEditorTool))
@@ -96,127 +131,155 @@ void SceneView::onMouseScroll(EVENT_HANDLER(MouseManager))
     if (!m_isFocused)
         return;
 
-    m_camZoom -= args->delta.Y( );
+    m_camZoom -= args->delta.Y();
 
-    if (m_camZoom < 1.0f) 
-        m_camZoom = 1.0f;
-
-    auto &camera = getEditorCamera( );
-
-    auto look = camera.GetLook( );
-
-    // normalize look and scale by zoom
-    look.Normalize( );
-    look = look * m_camZoom;
-
-    // negate the vector, opposite of look is going away from center
-    camera.SetPosition( camera.GetPosition( ) - look );
+    if (m_camZoom < 1) m_camZoom = 1.f;
 }
 
-void SceneView::onMouseMove(EVENT_HANDLER(MouseManager))
+void SceneView::UpdateCameraKeys(float dt)
 {
-    EVENT_ATTRS(MouseManager, MouseMoveArgs);
-
-    // ignore is not focused for pressing alt is down
-    if (!m_isFocused)
-        return;
-
-    // must have ctrl key down
-    if (!(GetCoreSystem( KeyboardManager )->GetModifiers( ) & KMD_CTRL ))
-        return;
-
-    auto &camera = getEditorCamera( );
-
-    auto look = camera.GetLook( );
-
-    auto positionDelta = args->positionDelta;
-
-    auto camTransform = camera.GetViewMatrix( );
-
-    // we need to limit the up delta so that we can't wrap if we are at the very top/bottom
-    positionDelta /= 2.f;
-
-    // must have left button down
-    if (sender->IsButtonDown( MBTN_LEFT ))
-    {
-        if (positionDelta.Length( ) > 0)
-        {
-            // generate the up rotation
-            auto upRotation = SQuat( 
-                positionDelta.Y( ), 
-                camTransform.TransformVector( SVec3( 1.0f, 0.0f, 0.0f ) ) 
-            );
-
-            // generate side rotation
-            auto sideRotation = SQuat( 
-                positionDelta.X( ), 
-                SVec3( 0.0f, 1.0f, 0.0f ) 
-            );
-
-            // transform w/ rotations
-            look = sideRotation.Rotate( look );
-            look = upRotation.Rotate( look );
-
-            camera.SetLook( look );
-        }
-    }
-}
-
-void SceneView::onKeyDown(EVENT_HANDLER(KeyboardManager))
-{
-    EVENT_ATTRS(KeyboardManager, KeyboardKeyArgs);
-
-    // the scene view doesn't have focus
-    if (!m_isFocused)
-        return;
+    auto *keyboardMgr = GetCoreSystem(KeyboardManager);
 
     float speed = 3;
 
-    auto &camera = getEditorCamera( );
+    //get the camera
+    GFXCamera &cam = getEditorCamera();
+    SVec3 look = cam.GetLook();
 
-    auto look = camera.GetLook( );
-    auto right = camera.GetRight( );
+    ///////////////////////////////////////////////////////////////////
+    // KEYBOARD MOVEMENT //////////////////////////////////////////////
+    //get other camera data, now that the look was set
+    auto right = cam.GetRight();
+    auto pos = m_camPos;
+    auto up = cam.GetUp();
+    auto dir = SVec3(0, 0, 0);
 
-    auto position = camera.GetPosition( );
-
-    auto up = camera.GetUp( );
-
-    auto direction = SVec3( 0.0f, 0.0f, 0.0f );
-
-    switch (args->key)
+    //update position
+    if (keyboardMgr->IsDown(KEY_W))
     {
-    case KEY_W:
-        direction += look;
-        break;
-    case KEY_S:
-        direction -= look;
-        break;
-    case KEY_A:
-        direction += right;
-        break;
-    case KEY_D:
-        direction -= right;
-        break;
-    case KEY_E:
-        direction += up;
-        break;
-    case KEY_Q:
-        direction -= up;
-        break;
-    default:
-        break;
+        dir += look;
+    }
+    if (keyboardMgr->IsDown(KEY_S))
+    {
+        dir -= look;
+    }
+    if (keyboardMgr->IsDown(KEY_A))
+    {
+        dir += right;
+    }
+    if (keyboardMgr->IsDown(KEY_D))
+    {
+        dir -= right;
     }
 
-    // make sure something happened
-    if (direction.Length( ) > 0.0f)
+    if (keyboardMgr->IsDown(KEY_E))
     {
-        // normalize vector, scale by speed
-        direction.Normalize( );
-        direction *= speed;
-
-        // apply to position
-        position += direction;
-
-        camera.SetPosition( position - look );
+        dir += up;
     }
+    if (keyboardMgr->IsDown(KEY_Q))
+    {
+        dir -= up;
+    }
+
+    //make sure something happened
+    if (dir.Length() > 0)
+    {
+        //normalize vector, scale by dt and speed
+        dir.Normalize();
+        dir *= dt * speed;
+
+        //apply to position
+        pos += dir;
+
+        //set new pos
+        m_camPos = pos;
+    }
+}
+
+void SceneView::UpdateCameraMouse(float dt)
+{
+    auto *mouseMgr = GetCoreSystem(MouseManager);
+
+    //get the camera
+    GFXCamera &cam = getEditorCamera();
+
+    SVec3 look = cam.GetLook();
+    SVec3 up = cam.GetUp();
+    SVec3 right = cam.GetRight();
+
+    ///////////////////////////////////////////////////////////////////
+    // CAMERA ROTATION
+    if (mouseMgr->IsButtonDown(MBTN_LEFT))
+    {
+        auto mouseDelta = mouseMgr->GetPositionDelta();
+        auto camTransform = cam.GetViewMatrix();
+
+        //we need to limit the up delta so that we can't wrap if we are at the very top/bottom
+        mouseDelta /= 2.f;
+
+        if (mouseDelta.Length() > 0)
+        {
+            //generate the up rotation
+            auto upRotation = SQuat(-mouseDelta.Y(), camTransform.TransformVector(SVec3(1.0, 0.0, 0.0)));
+
+            //generate side rotation
+            auto sideRotation = SQuat(-mouseDelta.X(), SVec3(0, 1, 0));
+
+            //transform w/ rotations
+            look = sideRotation.Rotate(look);
+            look = upRotation.Rotate(look);
+
+            cam.SetLook(look);
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    // PANNING
+    else if (mouseMgr->IsButtonDown(MBTN_MIDDLE))
+    {
+        auto mouseDelta = mouseMgr->GetPositionDelta();
+
+        if (mouseDelta.Length() > 0)
+        {
+            float width, height;
+
+            cam.GetDimensions(width, height);
+
+            m_camPos += right * -mouseDelta.X() * dt * width;
+
+            m_camPos += up * -mouseDelta.Y() * dt * height;
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    // ZOOM
+    else if (mouseMgr->IsButtonDown(MBTN_RIGHT))
+    {
+        auto mouseDelta = mouseMgr->GetPositionDelta();
+
+        if (mouseDelta.Length() > 0)
+        {
+            m_camZoom += -mouseDelta.Y() * dt;
+
+            if (m_camZoom < 1)
+            {
+                m_camZoom = 1;
+            }
+        }
+    }
+
+    //if in here, draw the center mark
+    float halfSize = 0.3f;
+
+    auto gfx = GetCoreSystem( GfxAPI );
+
+    gfx->DrawingMgr.SetColor(1, 0, 0, 1);
+    gfx->DrawingMgr.DrawLine(m_camPos - SVec3(halfSize, 0, 0), m_camPos + SVec3(halfSize, 0, 0));
+    gfx->DrawingMgr.DrawPoint(m_camPos + SVec3(halfSize, 0, 0));
+    gfx->DrawingMgr.SetColor(0, 1, 0, 1);
+    gfx->DrawingMgr.DrawLine(m_camPos - SVec3(0, halfSize, 0), m_camPos + SVec3(0, halfSize, 0));
+    gfx->DrawingMgr.DrawPoint(m_camPos + SVec3(0, halfSize, 0));
+    gfx->DrawingMgr.SetColor(0, 0, 1, 1);
+    gfx->DrawingMgr.DrawLine(m_camPos - SVec3(0, 0, halfSize), m_camPos + SVec3(0, 0, halfSize));
+    gfx->DrawingMgr.DrawPoint(m_camPos + SVec3(0, 0, halfSize));
 }
