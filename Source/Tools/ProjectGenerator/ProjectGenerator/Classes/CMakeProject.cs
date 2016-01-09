cbad8511b1cbd2e0232ae71ecd2f234645d8853a
Austin Brunkhorst <contact@austinbrunkhorst.com>
2016-01-08 20:08:32 -0800

    Added project generator

---
+++ Source/Tools/ProjectGenerator/ProjectGenerator/Classes/CMakeProject.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGenerator.Classes
+{
+    public class CMakeProject
+    {
+        /// <summary>
+        /// CMake Source directory for the instance of an application
+        /// </summary>
+        public static string SourceDirectory { get; set; }
+
+        /// <summary>
+        /// CMake module directory for the instance of an application
+        /// </summary>
+        public static string ModuleDirectory { get; set; }
+
+        /// <summary>
+        /// Absolute path to the CMakeLists.txt file
+        /// </summary>
+        public string Path { get; private set; }
+
+        public string[] PathDirectories;
+
+        /// <summary>
+        /// Path relative to the root directory, without the
+        /// CMakeLists.txt file
+        /// </summary>
+        public string DisplayName { get; private set; }
+
+        public CMakeProject(string path)
+        {
+            Path = path.Replace( '/', System.IO.Path.PathSeparator );
+
+            DisplayName = path
+                .Replace( SourceDirectory + "\\", "" )
+                .Replace( "\\", "/")
+                .Replace( "/CMakeLists.txt", "" );
+
+            PathDirectories = DisplayName.Split( '/' ).ToArray( );
+        }
+    }
+}
---
+++ Source/Tools/ProjectGenerator/ProjectGenerator/Forms/MainForm.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using ProjectGenerator.Classes;
+
+namespace ProjectGenerator.Forms
+{
+    public partial class MainForm : Form
+    {
+        private readonly Brush brushBGEven;
+        private readonly Brush brushBGOdd;
+
+        public MainForm()
+        {
+            InitializeComponent( );
+
+            brushBGEven = Brushes.White;
+            brushBGOdd = new SolidBrush( Color.FromArgb( 243, 243, 243 ) );
+
+            projectList.DrawItem += drawProjectItem;
+            projectList.MouseDoubleClick += onProjectDoubleClick;
+
+            reloadProjects( );
+        }
+
+        private void onProjectDoubleClick(object sender, MouseEventArgs e)
+        {
+            var index = projectList.IndexFromPoint( e.Location );
+
+            if (index == ListBox.NoMatches)
+                return;
+
+            var project = (CMakeProject)projectList.Items[ index ];
+
+            var form = new ProjectForm( project );
+
+            form.ShowDialog( );
+        }
+
+        private void reloadProjects()
+        {
+            var projectFiles = Directory.GetFiles( 
+                CMakeProject.SourceDirectory, 
+                "CMakeLists.txt",
+                SearchOption.AllDirectories
+            );
+
+            var projects = new ArrayList( );
+
+            foreach (var path in projectFiles)
+            {
+                var project = new CMakeProject( path );
+
+                projects.Add( project );
+            }
+
+            projectList.DataSource = projects;
+        }
+
+        private void drawProjectItem(object sender, DrawItemEventArgs e)
+        {
+            e.Graphics.FillRectangle( 
+                (e.Index & 1) == 0 ? brushBGEven : brushBGOdd, 
+                e.Bounds 
+            );
+
+            var project = (CMakeProject)projectList.Items[ e.Index ];
+
+            var directoryBrush = Brushes.Black;
+            var slashBrush = Brushes.Gray;
+
+            float slashWidth = e.Graphics.MeasureString( "/", e.Font ).Width;
+            float x = e.Bounds.X;
+
+            int i = 0;
+
+            foreach (var directory in project.PathDirectories)
+            {
+                var size = e.Graphics.MeasureString( directory, e.Font );
+
+                e.Graphics.DrawString( 
+                    directory, 
+                    e.Font, 
+                    directoryBrush, 
+                    x, 
+                    e.Bounds.Y + 5 
+                );
+
+                x += size.Width;
+
+                if (i < project.PathDirectories.Length - 1)
+                {
+                    e.Graphics.DrawString(
+                        "/",
+                        e.Font,
+                        slashBrush,
+                        x,
+                        e.Bounds.Y + 5
+                    );
+
+                    x += slashWidth;
+                }
+
+                ++i;
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            reloadProjects( );
+        }
+    }
+}
