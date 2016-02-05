f07d7c66c61ec26d8e7c4f8ef2bb393c84974a76
Jordan Ellis <jordanellis6809@aol.com>
2016-02-05 03:07:53 -0800

    Camera animator setup

---
+++ Source/Shared/Source/Components/CameraAnimation/CameraAnimatorComponent.h
@@ -0,0 +1,66 @@
+/* ----------------------------------------------------------------------------
+** Team Bear King
+** © 2015 DigiPen Institute of Technology, All Rights Reserved.
+**
+** CameraAnimatorComponent.h
+**
+** Author:
+** - Jordan Ellis - j.ellis@digipen.edu
+**
+** Contributors:
+** - <list in same format as author if applicable>
+** --------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <Component.h>
+
+class CameraAnimatorNode;
+
+namespace ursine
+{
+	namespace ecs
+	{
+		class Camera;
+	}
+}
+
+class CameraAnimator : public ursine::ecs::Component
+{
+	NATIVE_COMPONENT;
+
+	friend class CameraAnimatorSystem;
+
+public:
+	EditorButton(
+		play,
+		"Play Animation"		
+	);
+
+	EditorButton(
+		reset,
+		"Reset"
+	);
+
+	CameraAnimator(void);
+	~CameraAnimator(void);
+
+	void Play(void);
+	void Reset(void);
+
+private:
+
+	void updateAnimation(CameraAnimatorNode *node);
+	void updateAnimation(CameraAnimatorNode *node1, CameraAnimatorNode *node2, float t);
+
+	void enableDeletionNodes(bool flag);
+
+	// The animation nodes
+	std::vector<CameraAnimatorNode*> m_nodes;
+	ursine::ecs::Camera *m_camera;
+
+	bool m_playing;
+	int m_index;
+	float m_time;
+
+} Meta(Enable);
--- Source/Shared/Source/SharedComponents.h
+++ Source/Shared/Source/SharedComponents.h
@@ -70,3 +70,9 @@
 
 #include "Components/LevelLogic/ElevatorLiftMoverComponent.h"
 
+/////////////////////////////////
+////     Camera Animation    ////
+/////////////////////////////////
+
+#include "Components/CameraAnimation/CameraAnimatorComponent.h"
+#include "Components/CameraAnimation/CameraAnimatorNodeComponent.h"
