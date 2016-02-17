728c04129b98bdf2c9e59cec2a4ef0563af284b3
Austin Brunkhorst <contact@austinbrunkhorst.com>
2016-02-16 17:03:52 -0800

    Fixed logging

--- Source/Ursine3D/include/Debugging/UrsineLogTools.hpp
+++ Source/Ursine3D/include/Debugging/UrsineLogTools.hpp
@@ -25,14 +25,14 @@ namespace ursine
         #endif
 
         #if (URSINE_OUTPUT_FILE)
-            auto file_out = fopen(URSINE_ERROR_LOG_FILE, "at");
+            auto fileOut = fopen( URSINE_ERROR_LOG_FILE, "at" );
 
-            Log(file_out, URSINE_ASSERTION_LOG_HEADER, CC_TEXT_YELLOW, file, function, line, format, args...);
+            Log( fileOut, URSINE_ASSERTION_LOG_HEADER, CC_TEXT_YELLOW, file, function, line, format, args... );
 
-            fclose(file_out);
+            fclose( fileOut );
         #endif
 
-            ExitError();
+            ExitError( );
         }
 
         template<typename... Args>
@@ -43,11 +43,11 @@ namespace ursine
         #endif
 
         #if (URSINE_OUTPUT_FILE)
-            auto file_out = fopen(URSINE_ERROR_LOG_FILE, "at");
+            auto fileOut = fopen( URSINE_ERROR_LOG_FILE, "at" );
 
-            Log(file_out, URSINE_WARNING_LOG_HEADER, CC_TEXT_YELLOW, file, function, line, format, args...);
+            Log( fileOut, URSINE_WARNING_LOG_HEADER, CC_TEXT_YELLOW, file, function, line, format, args... );
 
-            fclose(file_out);
+            fclose( fileOut );
         #endif
         }
 
@@ -59,53 +59,53 @@ namespace ursine
         #endif
 
         #if (URSINE_OUTPUT_FILE)
-            auto file_out = fopen(URSINE_ERROR_LOG_FILE, "at");
+            auto fileOut = fopen( URSINE_ERROR_LOG_FILE, "at" );
 
-            Log(file_out, URSINE_ERROR_LOG_HEADER, CC_TEXT_BRIGHT_RED, file, function, line, format, args...);
+            Log( fileOut, URSINE_ERROR_LOG_HEADER, CC_TEXT_BRIGHT_RED, file, function, line, format, args... );
 
-            fclose(file_out);
+            fclose( fileOut );
         #endif
 
-            ExitError();
+            ExitError( );
         }
 
         template<typename... Args>
-        void Log(FILE *handle, const std::string &title, 
+        void Log(FILE *handle, const std::string &title,
             ConsoleColor title_color, URSINE_FFL_ARGS, URSINE_LOG_FORMATTED)
         {
-            std::string dashes((46 - title.length()) / 2, '-');
+            std::string dashes( (46 - title.length( )) / 2, '-' );
 
-            SetConsoleColor(CC_TEXT_BRIGHT_WHITE);
+            SetConsoleColor( CC_TEXT_BRIGHT_WHITE );
 
-            fprintf(handle, "================================================\n");
+            fprintf( handle, "================================================\n" );
 
-            SetConsoleColor(CC_TEXT_WHITE);
+            SetConsoleColor( CC_TEXT_WHITE );
 
-            fprintf(handle, (dashes + " ").c_str());
+            fprintf( handle, (dashes + " ").c_str( ) );
 
-            SetConsoleColor(title_color);
+            SetConsoleColor( title_color );
 
-            fprintf(handle, title.c_str());
+            fprintf( handle, title.c_str( ) );
 
-            SetConsoleColor(CC_TEXT_WHITE);
+            SetConsoleColor( CC_TEXT_WHITE );
 
-            if ((title.length() & 1) == 1)
+            if ((title.length( ) & 1) == 1)
                 dashes += '-';
 
-            fprintf(handle, (" "+ dashes + "\n\n").c_str());
+            fprintf( handle, (" " + dashes + "\n\n").c_str( ) );
 
-            SetConsoleColor(CC_TEXT_BRIGHT_WHITE);
+            SetConsoleColor( CC_TEXT_BRIGHT_WHITE );
 
-            fprintf(handle, format.c_str(), args...);
-            fprintf(handle, "\n\n");
+            fprintf( handle, format.c_str( ), args... );
+            fprintf( handle, "\n\n" );
 
-            OutputInfo(handle, file, function, line);
+            OutputInfo( handle, file, function, line );
 
-            SetConsoleColor(CC_TEXT_BRIGHT_WHITE);
+            SetConsoleColor( CC_TEXT_BRIGHT_WHITE );
 
-            fprintf(handle, "================================================\n\n");
+            fprintf( handle, "================================================\n\n" );
 
-            SetConsoleColor(CC_TEXT_WHITE);
+            SetConsoleColor( CC_TEXT_WHITE );
         }
     }
 }
