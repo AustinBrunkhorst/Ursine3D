e4addc1618c1595fc6b0d7b5c1d220fd83a5b39b
artainex <eternalstarphj@gmail.com>
2016-02-28 20:10:21 -0800

    Jordan u r genius!

--- Source/Ursine3D/include/Game Engine/Scene/Component/Native Components/AnimatorComponent.h
+++ Source/Ursine3D/include/Game Engine/Scene/Component/Native Components/AnimatorComponent.h
@@ -1,16 +1,16 @@
-/* ---------------------------------------------------------------------------
-** Team Bear King
-** ?2015 DigiPen Institute of Technology, All Rights Reserved.
+/*---------------------------------------------------------------------------
+**Team Bear King
+**?2015 DigiPen Institute of Technology, All Rights Reserved.
 **
-** AnimatorComponent.h
+* *AnimatorComponent.h
 **
-** Author:
-** - Matt Yan - m.yan@digipen.edu
-** - Hyung Jun Park - park.hyungjun@digipen.edu
-**
-** Contributors:
-** - <list in same format as author if applicable>
-** -------------------------------------------------------------------------*/
+**Author:
+**- Matt Yan - m.yan@digipen.edu
+**- Hyung Jun Park - park.hyungjun@digipen.edu
+*
+**Contributors:
+**- <list in same format as author if applicable>
+**-------------------------------------------------------------------------*/
 
 #pragma once
 
@@ -21,229 +21,236 @@
 #include "AnimationBuilder.h"
 #include "Array.h"
 #include "Model3DComponent.h"
+#include "TransformComponent.h"
 
 namespace ursine
 {
-	namespace ecs
-	{		
-		class Animator;
+    namespace ecs
+    {
+        class Animator;
 
-		class StateBlender
-		{
-		public:
-			EditorField(
-				std::string currentState,
-				GetcurrState,
-				SetcurrState
-			);
+        class StateBlender
+        {
+        public:
+            EditorField(
+                std::string currentState,
+                GetcurrState,
+                SetcurrState
+                );
 
-			EditorField(
-				std::string futureState,
-				GetfutState,
-				SetfutState
-			);
+            EditorField(
+                std::string futureState,
+                GetfutState,
+                SetfutState
+                );
 
-			//Slider
-			// blending starting position of current state
-			EditorMeta(InputRange(0.0f, 1.0f, 0.01f, "{{value.toPrecision( 2 )}}"))
-			EditorField(
-				float currtransPos,
-				GetcurrTransPosRatio,
-				SetcurrTransPosRatio
-			);
+            //Slider
+            // blending starting position of current state
+            EditorMeta(InputRange(0.0f, 1.0f, 0.01f, "{{value.toPrecision( 2 )}}"))
+                EditorField(
+                    float currtransPos,
+                    GetcurrTransPosRatio,
+                    SetcurrTransPosRatio
+                    );
 
-			// blending end position of future state
-			EditorMeta(InputRange(0.0f, 1.0f, 0.01f, "{{value.toPrecision( 2 )}}"))
-			EditorField(
-				float futtransPos,
-				GetfutTransPosRatio,
-				SetfutTransPosRatio
-			);
+            // blending end position of future state
+            EditorMeta(InputRange(0.0f, 1.0f, 0.01f, "{{value.toPrecision( 2 )}}"))
+                EditorField(
+                    float futtransPos,
+                    GetfutTransPosRatio,
+                    SetfutTransPosRatio
+                    );
 
             Meta(Enable)
-			StateBlender(void);
+                StateBlender(void);
 
-			const std::string &GetcurrState(void) const;
-			void SetcurrState(const std::string& cstate);
+            const std::string &GetcurrState(void) const;
+            void SetcurrState(const std::string& cstate);
 
-			const std::string &GetfutState(void) const;
-			void SetfutState(const std::string& fstate);
+            const std::string &GetfutState(void) const;
+            void SetfutState(const std::string& fstate);
 
-			const float &GetcurrTransPosRatio(void) const;
-			void SetcurrTransPosRatio(const float& tPos);
+            const float &GetcurrTransPosRatio(void) const;
+            void SetcurrTransPosRatio(const float& tPos);
 
-			const float &GetfutTransPosRatio(void) const;
-			void SetfutTransPosRatio(const float& tPos);
+            const float &GetfutTransPosRatio(void) const;
+            void SetfutTransPosRatio(const float& tPos);
 
-			const unsigned int &GetcurrTransFrm(void) const;
-			void SetcurrTransFrm(const unsigned int& tFrm);
+            const unsigned int &GetcurrTransFrm(void) const;
+            void SetcurrTransFrm(const unsigned int& tFrm);
 
-			const unsigned int &GetfutTransFrm(void) const;
-			void SetfutTransFrm(const unsigned int& tFrm);
+            const unsigned int &GetfutTransFrm(void) const;
+            void SetfutTransFrm(const unsigned int& tFrm);
 
-			const StateBlender *GetStateBlenderByNames(const std::string& currst, const std::string& futst);
-			
-		private:
-			// name of current state
-			std::string m_currState;
-			// name of future state
-			std::string m_futState;
-			// these reperesent the position of the keyframe 
-			// ratio = 1 / total keyframe number.
-			// if there is 100 frames, ratio 0.01 represents the fist frame
-			float m_ctrnsRate;
-			float m_ftrnsRate;
-			// frame calculated by rates
-			unsigned int m_ctrnsFrm;
-			unsigned int m_ftrnsFrm;
+            const StateBlender *GetStateBlenderByNames(const std::string& currst, const std::string& futst);
 
-		} Meta(
-			Enable,
+        private:
+            // name of current state
+            std::string m_currState;
+            // name of future state
+            std::string m_futState;
+            // these reperesent the position of the keyframe 
+            // ratio = 1 / total keyframe number.
+            // if there is 100 frames, ratio 0.01 represents the fist frame
+            float m_ctrnsRate;
+            float m_ftrnsRate;
+            // frame calculated by rates
+            unsigned int m_ctrnsFrm;
+            unsigned int m_ftrnsFrm;
+
+        } Meta(
+            Enable,
             WhiteListMethods,
-			EnableArrayType,
-			DisplayName( "State Blender" )
-        );
+            EnableArrayType,
+            DisplayName("State Blender")
+            );
 
-		/////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////
 
-		class Animator : public Component
-		{
-			NATIVE_COMPONENT;
+        class Animator : public Component
+        {
+            NATIVE_COMPONENT;
 
-		public:
+        public:
 
-			EditorButton(
-				ImportAnimation,
-				"Import Animation"
-			);
+            EditorButton(
+                ImportAnimation,
+                "Import Animation"
+            );
 
-			EditorField(
-				std::string currentState,
-				GetCurrentState,
-				SetCurrentState
-			);
+            EditorButton(
+                ImportRig,
+                "Import Rig"
+            );
 
-			EditorField(
-				std::string animation,
-				GetAnimation,
-				SetAnimation
-			);
+            EditorField(
+                std::string currentState,
+                GetCurrentState,
+                SetCurrentState
+            );
 
-			EditorField(
-				std::string currentRig,
-				GetRig,
-				SetRig
-			);
+            EditorField(
+                std::string animation,
+                GetAnimation,
+                SetAnimation
+            );
 
-			EditorField(
-				bool loopAnimation,
-				IsLooping,
-				SetLooping
-			);
+            EditorField(
+                std::string currentRig,
+                GetRig,
+                SetRig
+            );
 
-			EditorField(
-				bool playAnimation,
-				IsPlaying,
-				SetPlaying
-			);
+            EditorField(
+                bool playAnimation,
+                IsPlaying,
+                SetPlaying
+            );
 
-			EditorField(
-				bool changeState,
-				IsStateChanging,
-				SetStateChanging
-			);
+            EditorField(
+                bool changeState,
+                IsStateChanging,
+                SetStateChanging
+            );
 
-			EditorField(
-				bool renderDebug,
-				IsDebug,
-				SetDebug
-			);
-			
-			EditorField(
-				float timeScalar,
-				GetTimeScalar,
-				SetTimeScalar
-			);
-			
+            EditorField(
+                bool renderDebug,
+                IsDebug,
+                SetDebug
+            );
+
+            EditorField(
+                float timeScalar,
+                GetTimeScalar,
+                SetTimeScalar
+            );
+
             Meta(Enable)
-			Animator(void);
-			~Animator(void);
+            Animator(void);
+            ~Animator(void);
 
-			void OnInitialize(void) override;
+            void OnInitialize(void) override;
 
-			// stick this in a system
-			void UpdateAnimation(const float dt);
+            // stick this in a system
+            void UpdateAnimation(const float dt);
+            
+            void Debugging(const AnimationRig& _rig, const std::vector<SMat4>& vec);
 
-			// getter / setter //////////////////////////////////////
-			bool IsPlaying(void) const;
-			void SetPlaying(const bool isPlaying);
+            // getter / setter //////////////////////////////////////
+            bool IsPlaying(void) const;
+            void SetPlaying(const bool isPlaying);
 
-			bool IsLooping(void) const;
-			void SetLooping(const bool isLooping);
+            bool IsStateChanging(void) const;
+            void SetStateChanging(const bool stateChange);
 
-			bool IsStateChanging(void) const;
-			void SetStateChanging(const bool stateChange);
+            bool IsDebug(void) const;
+            void SetDebug(const bool useDebug);
 
-			bool IsDebug(void) const;
-			void SetDebug(const bool useDebug);
-			
-			float GetTimeScalar(void) const;
-			void SetTimeScalar(const float scalar);
+            float GetTimeScalar(void) const;
+            void SetTimeScalar(const float scalar);
 
-			const std::string &GetRig(void) const;
-			void SetRig(const std::string &rig);
-			
-			float GetAnimationTimePosition(void) const;
-			void SetAnimationTimePosition(const float position);
+            const std::string &GetRig(void) const;
+            void SetRig(const std::string &rig);
 
-			const std::string &GetCurrentState(void) const;
-			void SetCurrentState(const std::string &state);
+            float GetAnimationTimePosition(void) const;
+            void SetAnimationTimePosition(const float position);
 
-			const std::string &GetStateName(void) const;
-			void SetStateName(const std::string &state);
+            const std::string &GetCurrentState(void) const;
+            void SetCurrentState(const std::string &state);
 
-			const std::string &GetAnimation(void) const;
-			void SetAnimation(const std::string &name);
+            const std::string &GetStateName(void) const;
+            void SetStateName(const std::string &state);
 
-			// Array of animation states
-			ursine::Array<ursine::AnimationState> stArray;
-			// Array of state blender
-			ursine::Array<ursine::ecs::StateBlender> stBlender;
+            const std::string &GetAnimation(void) const;
+            void SetAnimation(const std::string &name);
 
-			void UpdateState(AnimationState* currSt, const Animation* currAni,
-				AnimationState* futSt, const Animation* futAni, const float& dt, float& transFactor);
+            // Array of animation states
+            ursine::Array<ursine::AnimationState> stArray;
+            // Array of state blender
+            ursine::Array<ursine::ecs::StateBlender> stBlender;
 
-			void GetTransFrmByRatio(AnimationState& state, unsigned int& frameIndex, const float& ratio);
+            void UpdateState(AnimationState *currSt, const Animation *currAni,
+                AnimationState *futSt, const Animation *futAni, const float& dt, float& transFactor);
 
-			StateBlender *GetStateBlenderByNames(const std::string& currst, const std::string& futst);
-			// save and load
-			// => save both Arrays
-			// => when load model, don't just load these, but should also load the animation if it doesn't exist
-			
-		private:
-			bool m_playing;
-			bool m_looping;
-			bool m_debug;
-			bool m_changeState;
-			float m_speedScalar;
-			//std::string m_StateMachineName;
-			std::string m_Rig;
-			std::string m_curStName;
-			std::string m_futStName;
-			std::string m_animationName;
-			std::string m_stateName;
-			std::vector<Animation*> m_animlist;
+            void ChangeState(AnimationState *currSt, AnimationState *futSt,
+                const float &currloopTimePos, const float &futloopTimePos, 
+                const float &currNoloopTimePos, const float &futNoloopTimePos);
 
-			static void recursClearChildren(const std::vector< Handle<Transform> > &children);
-			void clearChildren(void);
-			void importAnimation(void);
-			
-		} Meta(
-            Enable, 
-            WhiteListMethods, 
-            DisplayName( "Animator" )
-        ) EditorMeta(
-              RequiresComponents( typeof( ursine::ecs::Model3D ) )
-        );
-	}
+            void GetTransFrmByRatio(AnimationState& state, unsigned int& frameIndex, const float& ratio);
+
+            StateBlender *GetStateBlenderByNames(const std::string& currst, const std::string& futst);
+            // save and load
+            // => save both Arrays
+            // => when load model, don't just load these, but should also load the animation if it doesn't exist
+
+        private:
+            bool m_playing;
+            bool m_debug;
+            bool m_changeState;
+            float m_speedScalar;
+
+            std::string m_rig;
+            ursine::ecs::Entity *m_rigRoot;
+            std::string m_curStName;
+            std::string m_futStName;
+            std::string m_animationName;
+            std::string m_stateName;
+            std::vector<Animation*> m_animlist;
+
+            static void recursClearChildren(const std::vector< Handle<Transform> > &children);
+            void clearChildren(void);
+            void importAnimation(void);
+            void importRig(void);
+            void createBoneEntities(ursine::ecs::Transform *parent, const AnimationBone &bone);
+
+            void updateRigTransforms(ursine::ecs::Component::Handle<Transform> boneTrans, const AnimationBone &boneData);
+
+        } Meta(
+            Enable,
+            WhiteListMethods,
+            DisplayName("Animator")
+            ) EditorMeta(
+                RequiresComponents(typeof(ursine::ecs::Model3D))
+                );
+    }
 }
