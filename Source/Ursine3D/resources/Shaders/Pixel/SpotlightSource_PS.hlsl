b5313e49661da542f6de230d1f253da090842beb
cehnehd3h <m.yan@digipen.edu>
2016-03-04 10:16:21 -0800

    oooooooooooooooops

--- Source/Ursine3D/resources/Shaders/Pixel/SpotlightSource_PS.hlsl
+++ Source/Ursine3D/resources/Shaders/Pixel/SpotlightSource_PS.hlsl
@@ -111,73 +111,73 @@ SURFACE_DATA UnpackGBuffer(int2 location)
 
 float3 CalcPoint(float3 position, Material material)
 {
-    //float3 toLight = -lightDirection;
-    //float3 pixelToCamera = -position;
-    //float3 lightToPixel = (position - lightPosition);
+    float3 toLight = -lightDirection;
+    float3 pixelToCamera = -position;
+    float3 lightToPixel = (position - lightPosition);
 
-    //// grab the length
-    //float distanceToPixel = length(lightToPixel);
+    // grab the length
+    float distanceToPixel = length(lightToPixel);
 
-    //// normalize vector from light to pixel
-    //lightToPixel /= distanceToPixel;
+    // normalize vector from light to pixel
+    lightToPixel /= distanceToPixel;
 
-    //// get the angle from the ray to the pixel and our main direction
-    //float angleInCone = (dot(lightToPixel, lightDirection));
-
-    //// calculate final attenuation for the angle
-    //float angleAttenuation = (angleInCone - outerAngle) /
-    //    (innerAngle - outerAngle);
-
-    //float distAttenuation = saturate(1.0f - (distanceToPixel / lightSize));
-
-    //// Blinn specular
-    //pixelToCamera = normalize(pixelToCamera);
-    //float3 HalfWay = normalize(pixelToCamera + toLight);
-    //float NDotH = saturate(dot(HalfWay, material.normal));
-    //float specularValue = max(pow(NDotH, material.specPow), 0);
-
-    //// diffuse scalar from normal
-    //float normalScalar = max(dot(material.normal, toLight), 0);
-
-    //// calculate final light color
-    //float3 finalLightColor = (diffuseColor.rgb + specularValue * material.specIntensity);
-
-    //// apply dotp, attenuation for distance and angle
-    //finalLightColor *= normalScalar * angleAttenuation * distAttenuation * intensity;
-
-    //return finalLightColor;
-
-
-    /////////////////////////////////////////////////////////////////////
-    float3 ToLight = lightPosition - position;
-    float3 ToEye = -position;
-    float DistToLight = length(ToLight);
-
-    // Phong diffuse
-    ToLight /= DistToLight; // Normalize
-    float NDotL = saturate(dot(ToLight, material.normal));
-    float3 finalColor = diffuseColor.rgb * NDotL;
-
-    // Blinn specular
-    ToEye = normalize(ToEye);
-    float3 HalfWay = normalize(ToEye + ToLight);
-    float NDotH = saturate(dot(HalfWay, material.normal));
-    finalColor += diffuseColor.rgb * pow(NDotH, material.specPow) *
-        material.specIntensity;
-
-    // Cone attenuation
     // get the angle from the ray to the pixel and our main direction
-    float angleInCone = (dot(ToLight, lightDirection));
+    float angleInCone = (dot(lightToPixel, lightDirection));
 
     // calculate final attenuation for the angle
-    float conAtt = (angleInCone - outerAngle) /
+    float angleAttenuation = (angleInCone - outerAngle) /
         (innerAngle - outerAngle);
 
-    float Attn = saturate(1.0f - (DistToLight / lightSize));
+    float distAttenuation = saturate(1.0f - (distanceToPixel / lightSize));
 
-    finalColor *= material.diffuseColor.xyz * Attn * conAtt;
+    // Blinn specular
+    pixelToCamera = normalize(pixelToCamera);
+    float3 HalfWay = normalize(pixelToCamera + toLight);
+    float NDotH = saturate(dot(HalfWay, material.normal));
+    float specularValue = max(pow(NDotH, material.specPow), 0);
 
-    return finalColor * intensity;
+    // diffuse scalar from normal
+    float normalScalar = max(dot(material.normal, toLight), 0);
+
+    // calculate final light color
+    float3 finalLightColor = (diffuseColor.rgb + specularValue * material.specIntensity);
+
+    // apply dotp, attenuation for distance and angle
+    finalLightColor *= normalScalar * angleAttenuation * distAttenuation * intensity;
+
+    return finalLightColor;
+
+
+    ///////////////////////////////////////////////////////////////////////
+    //float3 ToLight = lightPosition - position;
+    //float3 ToEye = -position;
+    //float DistToLight = length(ToLight);
+
+    //// Phong diffuse
+    //ToLight /= DistToLight; // Normalize
+    //float NDotL = saturate(dot(ToLight, material.normal));
+    //float3 finalColor = diffuseColor.rgb * NDotL;
+
+    //// Blinn specular
+    //ToEye = normalize(ToEye);
+    //float3 HalfWay = normalize(ToEye + ToLight);
+    //float NDotH = saturate(dot(HalfWay, material.normal));
+    //finalColor += diffuseColor.rgb * pow(NDotH, material.specPow) *
+    //    material.specIntensity;
+
+    //// Cone attenuation
+    //// get the angle from the ray to the pixel and our main direction
+    //float angleInCone = (dot(ToLight, lightDirection));
+
+    //// calculate final attenuation for the angle
+    //float conAtt = (angleInCone - outerAngle) /
+    //    (innerAngle - outerAngle);
+
+    //float Attn = saturate(1.0f - (DistToLight / lightSize));
+
+    //finalColor *= material.diffuseColor.xyz * Attn * conAtt;
+
+    //return finalColor * intensity;
 }
 
 float4 main(DS_OUTPUT In) : SV_TARGET
