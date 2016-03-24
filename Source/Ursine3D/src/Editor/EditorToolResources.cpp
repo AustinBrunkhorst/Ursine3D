a856ebe50d1b149f8d152ea4fd9473cc9624ce8d
Austin Brunkhorst <contact@austinbrunkhorst.com>
2016-03-24 04:36:54 -0700

    Merge progress

--- Source/Ursine3D/src/Editor/EditorToolResources.cpp
+++ Source/Ursine3D/src/Editor/EditorToolResources.cpp
@@ -13,7 +13,7 @@ namespace ursine
         const GUID IconDirectionalLight = RESOURCE_GUID( "26b0a80a-c829-4e8e-bce9-54d2c1c23369" );
         const GUID       IconPointLight = RESOURCE_GUID( "f3901624-b038-44fe-9905-0ce6b7a741d7" );
         const GUID        IconSpotLight = RESOURCE_GUID( "da205a6b-5619-4ef4-aa64-f1cb875ce87e" );
-        const GUID           IconCamera = RESOURCE_GUID( "c27e1f32-3890-4c6b-afd4-7a1574dda439" );
+        const GUID           IconCamera = RESOURCE_GUID( "8a06df59-c9f4-4cca-9eac-972fec8965ce" );
 
         const GUID ArchetypeTranslateTool = RESOURCE_GUID( "25b0a698-7e3d-44a5-a79d-c244d7017f9d" );
         const GUID ArchetypeScaleTool     = RESOURCE_GUID( "b4879bb4-f351-4570-939f-100ff6349f98" );
