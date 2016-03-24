b239a8b8dc81d3de0b888f83ee71b8a738b4ab3a
Jordan Ellis <jordanellis6809@gmail.com>
2016-03-23 23:00:25 -0700

    initial push

---
+++ Source/Shared/Source/AI/Boss/Components/SludgeshotProjectileComponent.cpp
@@ -0,0 +1,16 @@
+/* ---------------------------------------------------------------------------
+** Team Bear King
+** ?2016 DigiPen Institute of Technology, All Rights Reserved.
+**
+** SludgeshotProjectileComponent.cpp
+**
+** Author:
+** - Jordan Ellis - j.ellis@digipen.edu
+**
+** -------------------------------------------------------------------------*/
+
+#include "Precompiled.h"
+
+#include "SludgeshotProjectileComponent.h"
+
+NATIVE_COMPONENT_DEFINITION(SludgeshotProjectile);
---
+++ Source/Shared/Source/AI/Boss/Components/SludgeshotProjectileComponent.h
@@ -0,0 +1,17 @@
+/* ---------------------------------------------------------------------------
+** Team Bear King
+** ?2016 DigiPen Institute of Technology, All Rights Reserved.
+**
+** SludgeshotProjectileComponent.h
+**
+** Author:
+** - Jordan Ellis - j.ellis@digipen.edu
+**
+** -------------------------------------------------------------------------*/
+
+#include <Component.h>
+
+class SludgeshotProjectile : public ursine::ecs::Component
+{
+    
+};
