4af8a5a203208f9d4678baa83be42c5fe4396957
CJsion3 <cpayne2012@gmail.com>
2016-04-04 20:07:37 -0700

    Percent slider fix

--- Source/Shared/Source/PlayerLogic/CharacterController/CharacterControllerComponent.h
+++ Source/Shared/Source/PlayerLogic/CharacterController/CharacterControllerComponent.h
@@ -92,7 +92,7 @@ public:
         SetLockLooking
     );
 
-    EditorMeta(InputRange(0.0f, 1.0f, 0.1f, "{{(value * 100.0).toFixed( 2 )}} %"))
+    EditorMeta(InputRange(0.0f, 1.0f, 0.01f, "{{(value * 100.0).toFixed( 2 )}} %"))
     EditorField(
         float jumpDirectionScalar,
         GetJumpDirectionScalar,
